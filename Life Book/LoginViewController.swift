78e1c61cdf56684596c268878d53278bd01e75a6
Max Rogers <marfodub@gmail.com>
2017-08-03 15:12:51 -0500

    login/register flow bare bones

--- Life Book/LoginViewController.swift
+++ Life Book/LoginViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: button actions
+    @IBAction func registerTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "register", sender: nil)
     }
-
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        if usernameTextField.text != "" && passwordTextField.text != "" {
+            //TODO: hit api
+            performSegue(withIdentifier: "hooray", sender: nil)
+        } else {
+            //TODO: alert user to fill in fields
+        }
+    
+    }
+    
+    
+    // move to next textfield on done tapped
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return false
+    }
+    
+    
+    
 }
--- Life Book/RegisterViewController.swift
+++ Life Book/RegisterViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        emailTextField.delegate = self
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+        if emailTextField.text != "" && usernameTextField.text != "" && passwordTextField.text != "" {
+            //TODO: hit api
+            performSegue(withIdentifier: "hooray", sender: nil)
+        } else {
+            //TODO: alert user to fill fields
+        }
+    }
+
+    
+    // move to next textfield on done tapped
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return false
+    }
+    
 
 }
