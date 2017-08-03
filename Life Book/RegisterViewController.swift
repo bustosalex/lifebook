d2c22f36d85c3b5d04f44ce9ca664b85cbbed2e8
Max Rogers <marfodub@gmail.com>
2017-08-03 14:44:34 -0500

    Login/Register View Controllers

---
+++ Life Book/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Life Book
+//
+//  Created by Max Rogers on 8/3/17.
+//  Copyright © 2017 App Factory. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
